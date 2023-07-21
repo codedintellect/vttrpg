ef606a67e6edbc96693cd2d818680d46a0803e04
Dmitri <codedintellect@gmail.com>
2023-07-20 20:58:32 -0500

    Transitioning from Firebase to Supabase Added "Discord" social login option TODO: e-mail auth reimplementation needed

---
+++ src/supabase.js
@@ -0,0 +1,23 @@
+const s = supabase.createClient('https://blunder.letz.dev/proxy', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICAgInJvbGUiOiAiYW5vbiIsCiAgICAiaXNzIjogInN1cGFiYXNlIiwKICAgICJpYXQiOiAxNjg5NDgzNjAwLAogICAgImV4cCI6IDE4NDczMzY0MDAKfQ.FktL6yEOOVd3lgtNoxFMAGLxhsNHfR7UPmGoiMrgDWo');
+
+async function getLocalUser() {
+  const { data: { user } } = await s.auth.getUser();
+  return user;
+}
+
+async function signUpWithEmail() {
+  const { data, error } = await s.auth.signUp({
+    email: 'example@email.com',
+    password: 'example-password',
+  });
+  console.log(data, error);
+}
+
+async function oAuthSignIn(service) {
+  if (await getLocalUser()) return;
+
+  const { data, error } = await s.auth.signInWithOAuth({
+    provider: service
+  });
+  console.log(data, error);
+}
