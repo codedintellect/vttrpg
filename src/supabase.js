a5653721e0c0cc0cf489d1b4a8977bc768c74cd7
Dmitri <codedintellect@gmail.com>
2023-07-28 05:04:34 -0500

    Added username loading to webpage

--- src/supabase.js
+++ src/supabase.js
@@ -1,19 +1,38 @@
 const s = supabase.createClient('https://blunder.letz.dev/proxy', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICAgInJvbGUiOiAiYW5vbiIsCiAgICAiaXNzIjogInN1cGFiYXNlIiwKICAgICJpYXQiOiAxNjg5NDgzNjAwLAogICAgImV4cCI6IDE4NDczMzY0MDAKfQ.FktL6yEOOVd3lgtNoxFMAGLxhsNHfR7UPmGoiMrgDWo');
 
+uuid = '';
+
 async function getLocalUser() {
   const { data: { user } } = await s.auth.getUser();
   console.log(user);
   if (user) {
+    uuid = user.id;
     editCSS(".avatar", "background-image", `url(${user["user_metadata"]["avatar_url"]})`);
-    document.querySelector("body").innerHTML.replaceAll("%username%", user["user_metadata"]["full_name"]);
-    console.log(document.querySelector("body").innerHTML);
-    console.log(document.querySelector("body").innerHTML.indexOf("%username%"))
+    let username = await getUsername(uuid);
+    document.querySelector(".tag").innerHTML = username;
+    document.getElementById("username").value = username;
     document.querySelector('.user').classList.remove("hidden");
   } else {
     document.querySelector('.reg').classList.remove("hidden");
   }
   document.querySelector('.loading').classList.add("hidden");
   return user;
+}
+
+async function getUsername(id = uuid) {
+  const { data, error } = await s
+    .from('usernames')
+    .select("username")
+    .eq('id', id);
+  return data[0]["username"];
+}
+
+async function checkUsername(username) {
+  const { data, error } = await s
+    .from('usernames')
+    .select("username")
+    .eq('username', username);
+  return data.length == 0;
 }
 
 async function signUpWithEmail(_email, _password) {
